3cb662149d269e7d89063a0b120d694d15ffa964
mkoertshuis <mickkoertshuis@gmail.com>
2024-12-02 16:33:04 +0100

    cleaned

--- events/day2/question1.py
+++ events/day2/question1.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from utils.network import get_input
 from utils.parser import split_input
 import numpy as np
@@ -18,6 +19,7 @@ def check_safe(levels: np.ndarray[int], error: int = 0) -> int:
         return 1
 
     if error > 0:
+        # Little bit of a bruteforce, would be better to detect the error and check safe if the error is removed
         for i in range(len(levels)):
             levels_with_one_removed = np.delete(levels, i)
             if check_safe(levels_with_one_removed, error - 1) == 1:
@@ -33,4 +35,8 @@ def sum_safe(arr: list[np.ndarray[int]], error: int = 0) -> int:
 if __name__ == "__main__":
     raw_input = get_input(2, 2024)
     arr = parse(raw_input)
-    print("Question 1:", sum_safe(arr))
+    start = datetime.now()
+    answer = sum_safe(arr)
+    end = datetime.now()
+
+    print(f"Question 1: {answer} in {(end-start).microseconds} ms")
--- events/day2/question2.py
+++ events/day2/question2.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from events.day2.question1 import parse, sum_safe
 from utils.network import get_input
 
@@ -5,4 +6,8 @@ from utils.network import get_input
 if __name__ == "__main__":
     raw_input = get_input(2, 2024)
     arr = parse(raw_input)
-    print("Question 2:", sum_safe(arr, 1))
+    start = datetime.now()
+    answer = sum_safe(arr, 1)
+    end = datetime.now()
+
+    print(f"Question 2: {answer} in {(end-start).microseconds} ms")
